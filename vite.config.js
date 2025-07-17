5f1da09498174e2314112b7581e10c584007e1d6
Sarvesh23923 <sarvesh23923@gmail.com>
2025-07-18 00:47:33 +0530

    Deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Young-wealth/', // Replace with your repository name
+  base: '/Young-Wealth/', // Updated to match your GitHub repository name exactly
 })
