d1f97ec68d90550fff5872d7dc87af552098fa2d
Sarvesh23923 <sarvesh23923@gmail.com>
2025-07-18 00:53:57 +0530

    Changes

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Young-Wealth/', // Updated to match your GitHub repository name exactly
+  base: '/young-wealth/', // Updated to match the GitHub repository URL case
 })
